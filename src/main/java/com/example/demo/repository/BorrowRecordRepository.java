101216c6bb121e4033b92d1ba95520944a03ec0b
Rohul Ray Edward S <166890725+Hashcat07@users.noreply.github.com>
2026-07-17 12:40:53 +0530

    feat: add custom methods

--- src/main/java/com/example/demo/repository/BorrowRecordRepository.java
+++ src/main/java/com/example/demo/repository/BorrowRecordRepository.java
@@ -1,7 +1,13 @@
 package com.example.demo.repository;
 
 import com.example.demo.entity.BorrowRecord;
+import com.example.demo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface BorrowRecordRepository extends JpaRepository<BorrowRecord,Long> {
+    Optional<BorrowRecord> findByIdAndReturnDateIsNull(Long id);
+    List<BorrowRecord> findByUserId(User bookId);
 }

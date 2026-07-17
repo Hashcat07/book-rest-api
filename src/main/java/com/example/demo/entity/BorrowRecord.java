07f5ae675ad0292bf22dd82e8d91deb89b217569
Rohul Ray Edward S <166890725+Hashcat07@users.noreply.github.com>
2026-07-17 10:28:44 +0530

    feat: add user entity and Borrowed record entity

---
+++ src/main/java/com/example/demo/entity/BorrowRecord.java
@@ -0,0 +1,35 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "borrow_record")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BorrowRecord {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id",nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id",nullable = false)
+    private Book book;
+
+    @Column(nullable = false)
+    private LocalDateTime borrowDate;
+
+    @Column
+    private LocalDateTime returnDate;
+
+}
---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,25 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Table(name = "users")
+@AllArgsConstructor
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false,unique = true)
+    private String email;
+
+
+}
